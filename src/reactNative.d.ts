6dc7e65fc8eef0803f88833403dc9e450b795bae
Louis Lagrange <lagrange.louis@gmail.com>
2025-02-02 23:46:44 +0100

    Remove android custom Alert

--- src/reactNative.d.ts
+++ src/reactNative.d.ts
@@ -5,22 +5,12 @@ declare module "react-native" {
     export const Platform: PlatformStatic;
 
     export interface NativeEventSubscription {
-        /**
-         * Call this method to un-subscribe from a native-event
-         */
         remove(): void;
     }
 
     type AppStateStatus = string;
-
     export interface AppStateStatic {
         currentState: AppStateStatus;
-        isAvailable: boolean;
-
-        /**
-         * Add a handler to AppState changes by listening to the change event
-         * type and providing the handler
-         */
         addEventListener(
             type: AppStateEvent,
             listener: (state: AppStateStatus) => void,
@@ -28,22 +18,12 @@ declare module "react-native" {
     }
     export const AppState: AppStateStatic;
 
-    /**
-     * Interface for NativeModules which allows to augment NativeModules with type informations.
-     * See react-native-sensor-manager for example.
-     */
-    interface NativeModulesStatic {
-        [name: string]: any;
+    export interface AlertButton {
+        text?: string;
+        onPress?: () => void;
     }
-
-    /**
-     * Native Modules written in ObjectiveC/Swift/Java exposed via the RCTBridge
-     * Define lazy getters for each module. These will return the module if already loaded, or load it if not.
-     * See https://reactnative.dev/docs/native-modules-ios
-     * @example
-     * const MyModule = NativeModules.ModuleName
-     */
-    export const NativeModules: NativeModulesStatic;
-
-    export const Alert: any;
+    interface AlertStatic {
+        alert(title: string, message?: string, buttons?: AlertButton[]): void;
+    }
+    export const Alert: AlertStatic;
 }
