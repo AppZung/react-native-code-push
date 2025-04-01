7625c70eb6e2dd8f75c538782275f05a65137f4a
Louis Lagrange <lagrange.louis@gmail.com>
2025-04-01 12:07:13 +0200

    fix: return restartApp promise

--- src/restartApp.ts
+++ src/restartApp.ts
@@ -7,6 +7,6 @@ import { NativeRNAppZungCodePushModule } from './internals/NativeRNAppZungCodePu
  *
  * @param onlyIfUpdateIsPending Indicates whether you want the restart to no-op if there isn't currently a pending update.
  */
-export async function restartApp(onlyIfUpdateIsPending = false) {
-  NativeRNAppZungCodePushModule.restartApp(onlyIfUpdateIsPending);
+export function restartApp(onlyIfUpdateIsPending = false) {
+  return NativeRNAppZungCodePushModule.restartApp(onlyIfUpdateIsPending);
 }
